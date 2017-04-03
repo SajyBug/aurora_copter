52f9732e1bb8e51582f47bb7aaa45af30e91016e
SajjdRahnama <sajjadmuscle@gmail.com>
2017-04-03 17:42:13 +0430

    save video in our directory

--- src/camera_topic.cpp
+++ src/camera_topic.cpp
@@ -28,16 +28,20 @@ int main(int argc, char** argv)
   image_pub_ = it_.advertise("/raspberry_pi/image_raw", 1);
   cv_bridge::CvImage Cvptr;
   Cvptr.encoding = sensor_msgs::image_encodings::BGR8;
-VideoCapture cap("http://192.168.7.10:8080/?action=stream.mjpg");
- // VideoCapture cap("http://192.168.43.137:4747/mjpegfeed?320x240.mjpeg");
-//VideoCapture cap("http://192.168.7.104:4747/mjpegfeed?320x240.mjpeg");
+  VideoCapture cap("http://192.168.7.10:8080/?action=stream.mjpg");
+  // VideoCapture cap("http://192.168.43.137:4747/mjpegfeed?320x240.mjpeg");
+  //VideoCapture cap("http://192.168.7.104:4747/mjpegfeed?320x240.mjpeg");
+  int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+  VideoWriter video("out.avi", CV_FOURCC('M', 'J', 'P', 'G'), 25, Size(frame_width, frame_height), true);
   while (cap.isOpened())
   {
     cap >> frame;
     Cvptr.image = frame;
     image_pub_.publish(Cvptr.toImageMsg());
+    video.write(frame);
     imshow("video", frame);
-    if (waitKey(30) >= 0)
+    if (waitKey(3) >= 0)
     {
       cv::destroyAllWindows();
       break;
